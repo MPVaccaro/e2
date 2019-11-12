22b6fc32ef5d76199b369037fcf1c7cf446d8fba
Mary Vaccaro <mpvaccaro@aol.com>
2019-11-12 12:19:12 -0500

    I am updating zipfoods

--- zipfoods/app/Controllers/ProductController.php
+++ zipfoods/app/Controllers/ProductController.php
@@ -1,14 +1,50 @@
 <?php
 namespace App\Controllers;
 
+use App\Products;
+
 class ProductController extends Controller
 {
-    /**
-     *
-     */
+    private $products;
+    
+    public function __construct($app)
+    
+    { 
+            parent::__construct($app);
+        
+            $this->products = new Products($this->app->path('database/products.json'));
+    }
+        
     public function index()
     {        
-        return 'Show all the products here...';
+//        dump($products);
+        
+//        return 'Show all the products here...';
+        
+//        $products = new Products($this->app->path('database/products.json'));
+        
+           return $this->app->view('products.index', ['products' => $this->products->getAll()]);
     }
     
+   
+    public function show() 
+    {
+        
+//        $products = new Products($this->app->path('database/products.json'));
+//        return 'Show details for an individual product...';
+        
+        
+        $id = $this->app->param('id');
+        
+        $product = $this->products->getById($id);
+        
+//        dump($product);
+        if (is_null($product))
+            {
+            return $this->app->view('products.missing', [ 'id' => $id ]);
+            } 
+        
+        
+        return $this->app->view('products.show', [ 'product' => $product ]); 
+    }
 }
--- zipfoods/routes.php
+++ zipfoods/routes.php
@@ -4,5 +4,6 @@ return [
     '/' => ['AppController', 'index'],
     '/contact' => ['AppController', 'contact'],
     '/about' => ['AppController', 'about'],
+    '/product' => ['ProductController', 'show'],
     '/products' => ['ProductController', 'index'],
 ];
